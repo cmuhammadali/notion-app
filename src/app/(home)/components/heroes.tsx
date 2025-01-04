545df7dc66260507094c3f32831d7f935e1d1208
Muhammadali <118815291+cMuhammadali@users.noreply.github.com>
2025-01-04 11:45:33 +0500

    auth

--- src/app/(home)/components/heroes.tsx
+++ src/app/(home)/components/heroes.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Loader } from "@/components/ui/loader";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Heroes = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <>
       <div className="max-w-3xl space-y-4">
@@ -12,10 +20,29 @@ export const Heroes = () => {
         <h3 className="text-base sm:text-xl md:text-2xl font-medium">
           Write. Plan. Collaborate. With a little help from AI.
         </h3>
-        <Button>
-          Get Notion free <ArrowRight className="h-4 w-4 ml-2" />
-        </Button>
-
+        {isLoading && (
+          <div className="flex w-full justify-center items-center">
+            <Loader />
+          </div>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button asChild>
+              <Link href={"/documents"}>
+                Enter Notion <ArrowRight className="h-4 w-4 ml-2" />
+              </Link>
+            </Button>
+          </>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button>
+                Get Notion Free <ArrowRight className="h-4 w-4 ml-2" />
+              </Button>
+            </SignInButton>
+          </>
+        )}
         <div className="flex flex-col items-center justify-center max-w-5xl">
           <div className="flex items-center">
             <div className="relative w-[400px] h-[400px] hidden md:block">
